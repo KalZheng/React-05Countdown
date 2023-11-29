2ba715e7c99d00ce2ba8951d545eda3a00bbd394
Kal Zheng <kal.zheng@inspire-dt.com>
2023-11-29 15:18:41 +0800

    use ref to get and update label from input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,18 @@
 import { useState, useRef } from "react";
 
 export default function Player() {
-  // const playerName = useRef();
-  const [enterPlayerName, setEnterPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
+  const [enterPlayerName, setEnterPlayerName] = useState("");
 
-  function handleChange(event){
-    setSubmitted(false);
-    setEnterPlayerName(event.target.value);
+  function handleClick() {
+    setEnterPlayerName(playerName.current.value);
   }
 
-  function handleClick(){
-    setSubmitted(true);
-    // setEnterPlayerName(playerName,current.value);
-  }
-  
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enterPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enterPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input ref={playerName} type="text" onChange={handleChange} value={enterPlayerName}/>
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>

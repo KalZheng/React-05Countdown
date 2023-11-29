4cae2801e1592e7e7d9c02982538b295b05565b9
Kal Zheng <kal.zheng@inspire-dt.com>
2023-11-29 15:15:42 +0800

    add changes for get input and change display

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  // const playerName = useRef();
+  const [enterPlayerName, setEnterPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event){
+    setSubmitted(false);
+    setEnterPlayerName(event.target.value);
+  }
+
+  function handleClick(){
+    setSubmitted(true);
+    // setEnterPlayerName(playerName,current.value);
+  }
+  
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enterPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" onChange={handleChange} value={enterPlayerName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

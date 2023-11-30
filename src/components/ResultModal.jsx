4f486b9874bea810e1cf75e7843fa80b705c4d59
Kal Zheng <kal.zheng@inspire-dt.com>
2023-11-30 16:04:35 +0800

    fixed auto reset error and add reset function

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,31 +1,35 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({
-  result,
-  targetTime,
-}, ref) {
-    const dialog = useRef();
-    useImperativeHandle(ref, () => {
-        return {
-            open(){
-                //now this dialog tag can change 
-                // to something else and timer 
-                // challenge will not need to change 
-                // function caller name
-                dialog.current.showModal();
-            }
-        };
-    });
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        //now this dialog tag can change
+        // to something else and timer
+        // challenge will not need to change
+        // function caller name
+        dialog.current.showModal();
+      },
+    };
+  });
   return (
-    <dialog ref={dialog} className="result-modal" >
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You Lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>x seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
